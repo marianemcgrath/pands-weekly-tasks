d29ff4dbfd3c0e1c1732dbf556dfffc3beb3ad16
Mariane McGrath <marianemcgrath@gmail.com>
2025-03-16 21:59:20 +0000

    Update collatz.py

--- week_04/collatz.py
+++ week_04/collatz.py
@@ -6,32 +6,4 @@
 
 #The program end if the current value is one.
 
-while True:
-    try:
-        x = int(input("Please enter a positive integer: ")) 
-      
-        if x < 0:
-           raise ValueError   
-        break  
-    except ValueError:
-
-        print("That was not a positive integer. Please try again!")
-
-
-my_seq =[x]
-while  x > 1:
-   
-    if x % 2 == 0:  
-        x = x //2
-     
-        my_seq.append(x)
-        
- 
-    else:
-        x = (x * 3) + 1
-         
-        my_seq.append(x)
-
-print(*my_seq, sep = ", ")
-
 # Resource: https://www.angela1c.com/projects/problem_set_links/problem4/
