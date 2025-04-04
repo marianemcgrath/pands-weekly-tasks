7419089453c053d157e018f01d278bb909eaa3b4
Mariane McGrath <marianemcgrath@gmail.com>
2025-04-04 19:43:59 +0100

    task7_first attempt

---
+++ week_07/es.py
@@ -0,0 +1,14 @@
+# A program that reads in a text file and outputs the number of e's it contains
+# Author: Mariane McGrath
+
+
+
+with open("data.txt","r") as f:
+    read_data = f.read()
+
+words = f.split()
+print ("Word Count: ", len(e))
+
+
+# Resources: https://www.youtube.com/watch?v=kE90BE-jd1U
+# Resources: https://realpython.com/working-with-files-in-python/
