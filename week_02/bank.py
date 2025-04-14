2d18511d0f20f2670b01d833d2c50add6f5b680f
Mariane McGrath <marianemcgrath@gmail.com>
2025-04-14 19:12:24 +0100

    Task2_Edit

--- week_02/bank.py
+++ week_02/bank.py
@@ -1,19 +1,22 @@
-# bank.py
-# this program should read in two money amounts and add the two amounts
+# Task 2: Bank 
+# This program should read in two money amounts and add the two amounts
+
 # Author Mariane McGrath
 
-# Reading each amount (in cent)
+# First we read each amount (in cent)
 amount1= int(input("Enter amount1(in cent):"))
 amount2= int(input("Enter amount2(in cent):"))
 
- # Add the two amounts
+# Resource: https://docs.vultr.com/python/examples/add-two-numbers
+
+# Then we add the two amounts
 total = amount1 + amount2
 
-# Converting the total to euros and cents
+# We also need to convert the total to euros and cents
 euro = total // 100 # Giving the integer number
 cents = total % 100 # Calculating cents
 
-# Source: DeepSeek (Prompt: "How to calculate euro and cent conversion for 2 integer numbers in a very basic and clean code")
+# Resource: DeepSeek (Prompt: "How to calculate euro and cent conversion for 2 integer numbers in a very basic and clean code")
 
 # Showing the result of the function in a readable format in euros and cents
-print(f"The sum of these is €{euro}.{cents:02d}")
+print (f"The sum of these is €{euro}.{cents:02d}")
