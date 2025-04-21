0882270e2262acf0581e0d4eb3e243b780784d0f
Mariane McGrath <marianemcgrath@gmail.com>
2025-04-21 22:05:36 +0100

    task2_edit250421

--- week_02/bank.py
+++ week_02/bank.py
@@ -4,9 +4,11 @@
 # Author Mariane McGrath
 
 # Ask the user to enter the first amount in cents.
+
+amount1= int(input("Enter amount1(in cent):"))
+
 # input() shows the message requesting for the user input
 # int() converts the user input into an integer
-amount1= int(input("Enter amount1(in cent):"))
 
 # Resource: https://docs.python.org/3/library/functions.html
 
@@ -16,7 +18,7 @@ amount2= int(input("Enter amount2(in cent):"))
 # Define the total for later conversion
 total = amount1 + amount2                           # Add the two amounts (in cents)
 
-# Convert the total to euros and cents
+# Convert the total to fit the correct format, in euros and cents
 euro = total // 100                                 # Gives the integer number
 cents = total % 100                                 # Calculates cents
 
@@ -27,9 +29,10 @@ cents = total % 100                                 # Calculates cents
 # Resource: DeepSeek (Prompt: "How to calculate euro and cent conversion for 2 integer numbers in a very basic and clean code")
 # Resource: https://docs.python.org/3/tutorial/introduction.html#numbers
 
-# Shows the result of the function in a readable format in euros and cents
-# f-string used to allow different variables directly
-# :02d used to ensure formatting is correct, showing 2 digits for cent.
 print(f"The sum of these is €{euro}.{cents:02d}")
+
+# f-string used to allow different variables to be used and the result
+# of the function in a readable format in euros and cents
+# :02d used to ensure formatting is correct, showing 2 digits for cent.
 
 # Resource: https://docs.python.org/3/tutorial/inputoutput.html#formatted-string-literals
