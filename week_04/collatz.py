1c9279aad5e00516efa8c9ea82a0ee60cba41ad1
Mariane McGrath <marianemcgrath@gmail.com>
2025-04-23 17:08:32 +0100

    task4_edit250423

--- week_04/collatz.py
+++ week_04/collatz.py
@@ -3,36 +3,33 @@
 
 # Author: Mariane McGrath
 
-# Define Collatz sequence funtion
-def collatz(number):
-    # It continues looping until it reaches number 1
-    while number != 1:
-    
-    # If the result is an even number, divide by 2
-        if number % 2 == 0:                             
-            print(number // 2)                          # Print the result to show the sequence
-            return number // 2                          # Ensuring the loop stops after the first run
-    
-   # Otherwise, if it is an off number, it multiplies by 3 and adds 1 
-        elif number % 2 == 1:
-            print(number * 3 + 1)
-            return (number * 3 + 1)
 
-# This is the beginning of the program for the user
-try:
-    n = int (input ("Please enter a positive integer:"))
+# Define Collatz sequence funtion in which 'number' is starting integer
+def collatz(number):           
+    print (number)                          # Shows the first number in the sequence
 
-    # Continues calculations (loops) until it reaches number 1
-    while n !=1:
+    if number == 1:                         # If the number is equal to 1
+        return number                       # Then the sequence ends (as per Collatz "hypothesis", in which all starting numbers eventually reach 1)
+        
+    if number % 2 == 0:                     # If the number is even
+        return collatz (number//2)          # Divide it by 2
 
-    # Performs Collatz calculations until it reaches 1 
-        n = collatz (n)
+    else:                                   # If the number is odd
+        return collatz ((number * 3) + 1)   # Multiply number by 3 and add 1
 
-# Display error message if the user input is not a positive integer
-except ValueError:
-    print("Please enter a valid integer: ")
+# Resource: https://www.youtube.com/watch?v=1imEkPu-jGQ (A Recursive approach to the Collatz Sequence in Python - Solving "Automate the Boring Stuff" project using a recursive function)
+                                               
+while True:                                 # Infinite loop that inly exits if the valid input is entered
+    try:
+        number = int(input("Enter integer number:"))     # Ask the user for an integer number
+        if number <=0:                                   # If a negative integer is entered
+            print ("Please enter a POSITIVE INTEGER.")   # Error message is issued
+        else:                                            # If user input is a positive integer (as required)
+            collatz (number)                             # Then the program shows the sequence
+            break                                        # And loop ends
+    except ValueError:                                   # If any other value such as a string or a floating-point number is entered
+        print (" Something went wrong, please enter a valid integer") # An error message is displayed to user and loop continues.
 
+# Resource: https://stackoverflow.com/questions/33508034/making-a-collatz-program-automate-the-boring-stuff
 
-# Resources: https://stackoverflow.com/questions/33508034/making-a-collatz-program-automate-the-boring-stuff
-# Resources: https://docs.python.org/3/reference/compound_stmts.html#while
-# Resources: https://en.wikipedia.org/wiki/Collatz_conjecture
+# Resource: https://docs.python.org/3/reference/compound_stmts.html (The try statement)
