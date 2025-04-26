cb1b0bbcc3aee49f3543f0a4b71d8d60b3d210ba
Mariane McGrath <marianemcgrath@gmail.com>
2025-04-26 17:46:48 +0100

    task7_edit250426

--- week_07/es.py
+++ week_07/es.py
@@ -1,16 +1,29 @@
 # A program that reads in a text file and outputs the number of e's it contains
 # Author: Mariane McGrath
 
-# Create string 
+from pathlib import Path
+path = moby_dick.txt
 
-# Open the file text containing text in read mode
-with open("data.txt","r") as f:
 
-# Defining text as a string
-    text = f.read()
-    e_count = text.count('e')
-    print(e_count)
+import sys
+# Resource: https://stackoverflow.com/questions/61031785/syntax-error-using-sys-argv-as-argument-in-function (sys.arg function )
+try:
+    path = sys.argv [1]
+    with open (moby_dick.txt,"rt", encoding='utf-8') as file:                                # Open the file text containing text in read mode
+                                                                        # Text file downloaded from https://www.kaggle.com/datasets/gauravduttakiit/melville-moby-dick?resource=download
+                                                                        # Resource: https://www.w3schools.com/python/python_file_handling.asp
+        text = file.read()
+        e_count = text.lower().count('e')
+        print (f"The number of total 'e' letters on the file is"+ {e_count})
 
+except IndexError:
+    print("Error: Please provide a filename (e.g., python counter.py moby_dick.txt)")
+except FileNotFoundError:
+    print(f"Error: File '{sys.argv[1]}' not found. Please check:")
+    print("- The file exists in the same directory as your script")
+    print("- You spelled the filename correctly")
+except Exception as e:
+    print(f"An error occurred: {str(e)}")
 
 
 # Resources: https://www.youtube.com/watch?v=kE90BE-jd1U
