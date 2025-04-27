d7a6669671c20b34bd87463aaa263d90d59f0843
Mariane McGrath <marianemcgrath@gmail.com>
2025-04-27 17:18:04 +0100

    task2_edit250427

--- week_02/bank.py
+++ week_02/bank.py
@@ -1,4 +1,5 @@
-# Task 2: bank.py
+# Task 2: Bank
+
 # This program reads in two money amounts and add the two amounts
 
 # Author Mariane McGrath
@@ -6,7 +7,7 @@
 # Ask the user to enter the first amount in cents.
 amount1= int(input("Enter amount1(in cent):"))      # input() shows the message requesting for the user input
                                                     # int() converts the user input into an integer
-                                                    # Resource: https://docs.python.org/3/library/functions.html
+                                                    # Source: https://docs.python.org/3/library/functions.html
 
 # Ask the user to enter the second amount in cents.
 amount2= int(input("Enter amount2(in cent):"))      
@@ -21,9 +22,9 @@ cents = total % 100                                 # Calculates cents
                                                     # 1 euro = 100 cents, so dividing by 100 separates the two different units.
                                                     # Integer division (//), used to discard fractional euros.
                                                     # (%) used to isolate the remainder (cents) after extracting full euros.
-                                                    # Resource: DeepSeek (Prompt: "How to calculate euro and cent conversion for 2 integer numbers in a very basic and clean code")
-                                                    # Resource: https://docs.python.org/3/tutorial/introduction.html#numbers
+                                                    # Source: DeepSeek (Prompt: "How to calculate euro and cent conversion for 2 integer numbers in a very basic and clean code")
+                                                    # Source: https://docs.python.org/3/tutorial/introduction.html#numbers
 
 print(f"The sum of these is €{euro}.{cents:02d}")   # f-string used to allow different variables to be used and the result of the function in a readable format in euros and cents
-                                                    # :02d used to ensure formatting is correct, showing 2 digits for cent.
-                                                    # Resource: https://docs.python.org/3/tutorial/inputoutput.html#formatted-string-literals
+                                                    # The :02d is used to ensure formatting is correct, showing 2 digits for cent
+                                                    # Source: https://docs.python.org/3/tutorial/inputoutput.html#formatted-string-literals
